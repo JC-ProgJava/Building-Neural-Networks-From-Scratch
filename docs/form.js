165d644fd0ff1bd0811149bd19e587c4aad0c8ab
JC-ProgJava <juch5796@gmail.com>
2021-08-25 18:21:18 +0800

    Update form.js

--- docs/form.js
+++ docs/form.js
@@ -2,10 +2,10 @@ window.addEventListener('load', function() {
     const vals = document.getElementsByClassName("rating")[0].getElementsByTagName("label");
     for (var i = 0; i < vals.length; i++) {
         vals[i].addEventListener("click", function () {
-          this.submit();
+          document.getElementsByClassName("rating")[0].submit();
         });
     }
-    
+
     if (localStorage.hasOwnProperty('hasRated')) {
         hide();
     }
